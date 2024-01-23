0799a56d65002751a94747452337e49efff0176b
Baibhavos <baibhavkumar303@gmail.com>
2024-01-23 16:56:05 +0530

    1. Fixed: Minor bugs.   2. Frontend: Profile Page.

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ const storage = multer.diskStorage({
         cb( null, "public/assets");
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
+        cb(null, `${file.originalname}`);
     }
 });
 const upload = multer({ storage });
